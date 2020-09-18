264011a129a46d00818fd791ad424eb610fc9992
ullmans <shilo.ullman@gmail.com>
2020-09-18 17:53:24 +0300

    changed server.cpp to server.hpp

--- server.hpp
+++ server.hpp
@@ -1,15 +1,17 @@
+#pragma once
+
 #include <sys/socket.h>
 #include <string_view>
 #include <iostream>
+
+#include "ClientHandlar.hpp"
+
+
 namespace server_side {
+
     class Server {
         public:
-            virtual void open(int port);
+            virtual void open(int port, cliecnt_side::ClientHandlar);
             virtual void close();
-    };
-}
-namespace client_side {
-    class ClientHandler {
-        public:
     };
 };
