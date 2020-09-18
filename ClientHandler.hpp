22c77a528198069382aee91e15fee498872f2e15
Yair Koskas <yairi2003@gmail.com>
2020-09-18 21:43:08 +0300

    changed name

---
+++ ClientHandler.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <string_view>
+#include <iostream>
+#include <string>
+
+
+namespace client_side {
+    class ClientHandler {
+        public:
+            virtual int handleClient(std::string inputStream, std::string outputStream);
+    };
+};
