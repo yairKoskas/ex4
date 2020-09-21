7b8e6602c84e149616ae7cce0cb1199e2beff19f
Yair Koskas <yairi2003@gmail.com>
2020-09-21 13:39:04 +0300

    added a parallel server module, which will work with threads per client

---
+++ MyParallelServer.cpp
@@ -0,0 +1,44 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <iostream>
+#include <string_view>
+#include <system_error>
+#include <pthread.h>
+
+#include "ClientHandler.hpp"
+#include "MyParallelServer.hpp"
+
+#define THROW_SYSTEM_ERROR() \
+    throw std::system_error { errno, std::system_category() }
+
+
+void MyParallelServer::open(int port, client_side::ClientHandler ch) {
+    m_sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if(m_sockfd < 0) {
+        THROW_SYSTEM_ERROR();
+    }
+    sockaddr_in connectAddress{};
+    if (0 == inet_aton("127.0.0.1", &connectAddress.sin_addr)) {
+        closeServer();
+        throw std::runtime_error{"Failed converting IP to 4-bytes"};
+    }
+    connectAddress.sin_family = AF_INET;
+    connectAddress.sin_port = htons(port);
+    if (0 > connect(m_sockfd, reinterpret_cast<const sockaddr*>(&connectAddress),
+                    sizeof(connectAddress))) {
+        closeServer();
+        THROW_SYSTEM_ERROR();
+    }
+    while(true) {
+        
+    }
+}
+
+void MyParallelServer::closeServer() {
+    close(m_sockfd);
+} 
---
+++ MyParallelServer.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Server.hpp"
+
+class MyParallelServer : public server_side::Server {
+    private:
+        int m_sockfd;
+    public:
+        void open(int port, client_side::ClientHandler ch);
+        void closeServer();
+};
