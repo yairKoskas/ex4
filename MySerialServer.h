b6c61320f389d7e3e37c384a5e445d89d3fa583b
Yair Koskas <yairi2003@gmail.com>
2020-09-22 03:18:27 +0300

    vscode had a problem that was fixed by changing the file extention, i don't care

--- MySerialServer.h
+++ MySerialServer.h
@@ -6,7 +6,6 @@ class MySerialServer : public server_side::Server {
     private:
         int m_sockfd;
     public:
-        MySerialServer();
-        void open(int port);
-        void closeServer();
+        void open(int port, client_side::ClientHandler ch) override;
+        void closeServer() override;
 };
